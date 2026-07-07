9d22093a968b3a74e06672a1d9579af7a06d7fde
leonelmunimartins2015-maker <leonelmunimartins2015@gmail.com>
2026-07-07 20:56:28 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+# Configurações do BarberFlow
 
+NOME_SISTEMA = "BarberFlow"
+
+VERSAO = "1.0.0"
+
+MODO = "desenvolvimento"
+
+
+# Configurações gerais
+
+DIAS_AGENDAMENTO_FUTURO = 7
+
+WHATSAPP_ATIVO = True
