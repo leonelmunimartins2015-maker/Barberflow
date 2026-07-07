51e8952bdc32d737f037cb2172a9a69db89a7ae4
leonelmunimartins2015-maker <leonelmunimartins2015@gmail.com>
2026-07-07 20:57:32 -0300

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,1 +1,24 @@
+# Conexão com Firebase - BarberFlow
 
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+
+def iniciar_firebase():
+
+    try:
+        cred = credentials.Certificate(
+            "firebase_key.json"
+        )
+
+        firebase_admin.initialize_app(cred)
+
+        db = firestore.client()
+
+        return db
+
+    except Exception as erro:
+
+        print("Erro Firebase:", erro)
+
+        return None
