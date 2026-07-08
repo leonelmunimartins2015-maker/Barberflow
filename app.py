69365f6ac3166965fa0aebd0a296cdf6c2c3b357
leonelmunimartins2015-maker <leonelmunimartins2015@gmail.com>
2026-07-07 22:19:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,28 @@ def cadastrar_cliente():
     telefone = request.form["telefone"]
 
     return f"""
-    <h1>Cliente cadastrado!</h1>
-    <p>Nome: {nome}</p>
-    <p>Telefone: {telefone}</p>
-    <br>
-    <a href="/clientes">Voltar</a>
+    <!DOCTYPE html>
+    <html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <title>Cliente cadastrado</title>
+    </head>
+
+    <body style="background:#111;color:white;text-align:center;font-family:Arial">
+
+        <h1>✅ Cliente cadastrado!</h1>
+
+        <p>Nome: {nome}</p>
+        <p>Telefone: {telefone}</p>
+
+        <br>
+
+        <a href="/clientes" style="color:#c89b3c">
+            Voltar para clientes
+        </a>
+
+    </body>
+    </html>
     """
 
 
